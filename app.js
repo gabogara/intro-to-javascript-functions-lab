84f5232b5f7a4cfbaea6d91f0d0bb9b435fa888d
gabogara <gabo1gara@gmail.com>
2025-11-04 22:48:15 -0800

    Add solutions for exercise 9

--- app.js
+++ app.js
@@ -155,6 +155,44 @@ const convertTemperature = (value, scale) => {
   }
 };
 
-console.log("Exercise 8 Result:", convertTemperature(32, "C"));
-console.log("Exercise 8 Result:", convertTemperature(32, "F"));
-console.log("Exercise 8 Result:", convertTemperature(32, "V"));
+// console.log("Exercise 8 Result:", convertTemperature(32, "C"));
+// console.log("Exercise 8 Result:", convertTemperature(32, "F"));
+// console.log("Exercise 8 Result:", convertTemperature(32, "V"));
+
+//-----------------------------------------------------------------------------------------------------
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator = (a, b, operation) => {
+  if (operation === "add") {
+    return a + b;
+  } else if (operation === "subtract") {
+    return a - b;
+  } else if (operation === "multiply") {
+    return a * b;
+  } else if (operation === "divide") {
+    return a / b;
+  } else {
+    return "invalid operator";
+  }
+};
+
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "add"));
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "subtract"));
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "multiply"));
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "divide"));
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "other"));
